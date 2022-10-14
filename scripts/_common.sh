42e87e8b5bf425caa2dd4630a61b4ad38a2ad170
Victor Morales <chipahuac@hotmail.com>
2022-10-14 08:42:31 -0700

    Fix retrieval events

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -38,14 +38,8 @@ function get_status {
     grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage " %"}'
     printf "Memory free(Kb):"
     awk -v low="$(grep low /proc/zoneinfo | awk '{k+=$2}END{print k}')" '{a[$1]=$2}  END{ print a["MemFree:"]+a["Active(file):"]+a["Inactive(file):"]+a["SReclaimable:"]-(12*low);}' /proc/meminfo
-    if [[ ${INGRESS_CONTROLLER:-nginx} == "nginx" ]]; then
-        echo "Kubernetes Events (ingress-nginx):"
-        kubectl get events -n ingress-nginx --sort-by=".metadata.managedFields[0].time"
-        echo "Kubernetes Resources (ingress-nginx):"
-        kubectl get all -n ingress-nginx -o wide
-        echo "NGINX controller logs:"
-        kubectl logs -n ingress-nginx -l app.kubernetes.io/name=ingress-nginx -l app.kubernetes.io/component=controller
-    fi
+    echo "Kubernetes Events:"
+    kubectl alpha events
     echo "Kubernetes Resources:"
     kubectl get all -A -o wide
     echo "Kubernetes Nodes:"
