b3d0fdcca0ff202f8f47b9d58bed6cd8adfda7a7
Victor Morales <chipahuac@hotmail.com>
2022-11-22 14:28:33 -0800

    Enable Web UI kpt packages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,6 +22,7 @@ trap get_status ERR
 
 base_path=/opt/nephio
 system_path="$base_path/system"
+webui_path="$base_path/webui"
 configsys_path="$base_path/configsync"
 
 function get_pkg {
@@ -33,13 +34,22 @@ function get_pkg {
         sudo -E kpt pkg get --for-deployment "$url" "$path"
         sudo chown -R "$USER": "$path"
     fi
-    kpt pkg tree "$path"
+    if [[ ${DEBUG:-false} == "true" ]]; then
+        kpt pkg tree "$path"
+    fi
+}
+
+function kpt_apply {
+    kpt live apply "$1" --reconcile-timeout=15m
 }
 
 sudo mkdir -p "$base_path"
 
 # Install server components
 get_pkg system
+
+# Install Web UI
+get_pkg webui
 
 # Installing Config Sync in Workload Clusters
 get_pkg configsync
@@ -50,14 +60,17 @@ kpt fn eval "$configsys_path" \
     -- by-path=spec.git.repo by-value-regex='https://github.com/[a-zA-Z0-9-]+/(.*)' \
     put-value="https://github.com/${GITHUB_USERNAME}/\${1}"
 
-for path in "$system_path" "$configsys_path"; do
+for path in "$system_path" "$configsys_path" "$webui_path"; do
     kpt fn render "$path"
     kpt live init "$path" --force
 done
 
 for context in $(kubectl config get-contexts --no-headers --output name); do
     kubectl config use-context "$context"
-    path="$system_path"
-    [[ $context == *"edge"* ]] && path="$configsys_path"
-    kpt live apply "$path" --reconcile-timeout=15m
+    if [[ $context == "kind-nephio"* ]]; then
+        kpt_apply "$system_path"
+        kpt_apply "$webui_path"
+    else
+        kpt_apply "$configsys_path"
+    fi
 done
